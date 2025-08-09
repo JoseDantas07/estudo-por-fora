37e815aa5e6b1473b62aaca9255bbaaf57d5e32a
José Dantas <josedantastrabalho18@gmail.com>
2025-08-09 18:04:42 -0300

    Delete To-do-list directory

--- To-do-list/src/main/java/To_do_list/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package To_do_list.repository;
-
-import To_do_list.entity.TaskEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<TaskEntity,Long> {
-}
